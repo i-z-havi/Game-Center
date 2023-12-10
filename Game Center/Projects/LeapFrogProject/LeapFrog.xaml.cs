8add0edfe690735f102ddd0f9ea76b86e92799b2
i-z-havi <136263166+i-z-havi@users.noreply.github.com>
2023-12-10 15:15:36 +0200

    Cleaned up comments

--- Game Center/Projects/LeapFrogProject/LeapFrog.xaml.cs
+++ Game Center/Projects/LeapFrogProject/LeapFrog.xaml.cs
@@ -37,7 +37,6 @@ namespace Game_Center.Projects.LeapFrogProject
         private double score = 0;
         private double tries = 0;
 
-        //todo: graphics, better score system, game over on outer bounds, instructions on presentation page, maybe split into objects?
         public LeapFrog()
         {
             InitializeComponent();
@@ -61,10 +60,10 @@ namespace Game_Center.Projects.LeapFrogProject
         {
             Rect platformhitbox = new Rect(Canvas.GetLeft(lilyplatform), Canvas.GetTop(lilyplatform), lilyplatform.Width, lilyplatform.Height);
             TranslateTransform cubeTransform = FrogCube.RenderTransform as TranslateTransform;
-            if (cubeTransform!=null)
+            if (cubeTransform != null)
             {
-                Rect FrogCubehitbox = new Rect(Canvas.GetLeft(FrogCube)+cubeTransform.X, Canvas.GetTop(FrogCube)+cubeTransform.Y, FrogCube.Width, FrogCube.Height);
-                if (left.IntersectsWith(FrogCubehitbox)||right.IntersectsWith(FrogCubehitbox)||FrogCubehitbox.X<left.X||FrogCubehitbox.X>right.X)
+                Rect FrogCubehitbox = new Rect(Canvas.GetLeft(FrogCube) + cubeTransform.X, Canvas.GetTop(FrogCube) + cubeTransform.Y, FrogCube.Width, FrogCube.Height);
+                if (left.IntersectsWith(FrogCubehitbox) || right.IntersectsWith(FrogCubehitbox) || FrogCubehitbox.X < left.X || FrogCubehitbox.X > right.X)
                 {
                     Canvas.SetLeft(FrogCube, Canvas.GetLeft(FrogCube) + cubeTransform.X);
                     Canvas.SetTop(FrogCube, Canvas.GetTop(FrogCube) + cubeTransform.Y);
@@ -80,13 +79,13 @@ namespace Game_Center.Projects.LeapFrogProject
                     }
                 }
 
-                if ((platformhitbox.X-24<FrogCubehitbox.X&&FrogCubehitbox.X<platformhitbox.X+50&& (FrogCubehitbox.Y == platformhitbox.Y - 26)|| platformhitbox.IntersectsWith(FrogCubehitbox)))
+                if ((platformhitbox.X - 24 < FrogCubehitbox.X && FrogCubehitbox.X < platformhitbox.X + 50 && (FrogCubehitbox.Y == platformhitbox.Y - 26) || platformhitbox.IntersectsWith(FrogCubehitbox)))
                 {
                     score++;
                     Score.Text = $"Score: {score}";
-                    if (score==10)
+                    if (score == 10)
                     {
-                        if(MessageBox.Show("Congratulations! Your score was "+(int)(score/tries*100)+". Would you like to try again?","Game Over", MessageBoxButton.YesNo)==MessageBoxResult.Yes)
+                        if (MessageBox.Show("Congratulations! Your score was " + (int)(score / tries * 100) + ". Would you like to try again?", "Game Over", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
                         {
                             StartOver();
                         }
@@ -104,7 +103,6 @@ namespace Game_Center.Projects.LeapFrogProject
                         //checks to see if we move platform left or right
                         if (rnd.Next(2) == 0)
                         {
-                            //if we decide to move platform left:
                             //if the platform is too much to the left, we dont want it to move even further left. 
                             if (Canvas.GetLeft(lilyplatform) - 75 < 20)
                             {
@@ -132,7 +130,6 @@ namespace Game_Center.Projects.LeapFrogProject
                         platformhitbox.X = Canvas.GetLeft(lilyplatform);
                     }
                 }
-                    
             }
         }
 
@@ -172,7 +169,7 @@ namespace Game_Center.Projects.LeapFrogProject
 
         private void DrawVector(object sender, MouseEventArgs e)
         {
-            if (_vectorStarted&&!_isAnimated)
+            if (_vectorStarted && !_isAnimated)
             {
                 _line.Stroke = Brushes.White;
                 _referencePoint = e.GetPosition(this);
@@ -185,72 +182,56 @@ namespace Game_Center.Projects.LeapFrogProject
 
         private void DoubleAnimationUsingPathExample()
         {
-                //set is animated to true so that other functions can check state
-                _isAnimated= true;
-                PathFigure tempPath = new PathFigure();
-                tempPath.StartPoint = new Point(0, 0);
-                Point control = new Point();
-                control = Point.Add(tempPath.StartPoint, _vector);
-                Point endPoint = new Point(control.X + _vector.X, 0);
-                QuadraticBezierSegment qBezierSegment = new QuadraticBezierSegment(control, endPoint, true);
-                tempPath.Segments.Add(qBezierSegment);
-                NameScope.SetNameScope(this, new NameScope());
+            //set is animated to true so that other functions can check state
+            _isAnimated = true;
+            PathFigure tempPath = new PathFigure();
+            tempPath.StartPoint = new Point(0, 0);
+            Point control = new Point();
+            control = Point.Add(tempPath.StartPoint, _vector);
+            Point endPoint = new Point(control.X + _vector.X, 0);
+            QuadraticBezierSegment qBezierSegment = new QuadraticBezierSegment(control, endPoint, true);
+            tempPath.Segments.Add(qBezierSegment);
+            NameScope.SetNameScope(this, new NameScope());
 
-                // Create a transform. This transform
-                // will be used to move the rectangle.
-                TranslateTransform animatedTranslateTransform = new TranslateTransform();
+            // transform will move rectangle
+            TranslateTransform animatedTranslateTransform = new TranslateTransform();
 
-                // Register the transform's name with the page
-                // so that they it be targeted by a Storyboard.
-                this.RegisterName("AnimatedTranslateTransform", animatedTranslateTransform);
-                FrogCube.RenderTransform = animatedTranslateTransform;
+            // register transform so storyboard can target it
+            this.RegisterName("AnimatedTranslateTransform", animatedTranslateTransform);
+            FrogCube.RenderTransform = animatedTranslateTransform;
 
-                //create pathgeometry
-                PathGeometry animationPath = new PathGeometry();
-                animationPath.Figures.Add(tempPath);
+            //create pathgeometry
+            PathGeometry animationPath = new PathGeometry();
+            animationPath.Figures.Add(tempPath);
 
-                // Create a DoubleAnimationUsingPath to move the
-                // rectangle horizontally along the path by animating
-                // its TranslateTransform.
-                DoubleAnimationUsingPath translateXAnimation = new DoubleAnimationUsingPath();
-                translateXAnimation.PathGeometry = animationPath;
-                translateXAnimation.Duration = TimeSpan.FromSeconds(2);
+            //create doubleanimation path to move rectangle horizontally via translatetransform
+            DoubleAnimationUsingPath translateXAnimation = new DoubleAnimationUsingPath();
+            translateXAnimation.PathGeometry = animationPath;
+            translateXAnimation.Duration = TimeSpan.FromSeconds(2);
 
-                // Set the Source property to X. This makes
-                // the animation generate horizontal offset values from
-                // the path information.
-                translateXAnimation.Source = PathAnimationSource.X;
+            //source proprty to x (makes animation generate horizontal values)
+            translateXAnimation.Source = PathAnimationSource.X;
 
-                // Set the animation to target the X property
-                // of the TranslateTransform named "AnimatedTranslateTransform".
-                Storyboard.SetTargetName(translateXAnimation, "AnimatedTranslateTransform");
-                Storyboard.SetTargetProperty(translateXAnimation, new PropertyPath(TranslateTransform.XProperty));
+            // set animation to target x of translatetransform
+            Storyboard.SetTargetName(translateXAnimation, "AnimatedTranslateTransform");
+            Storyboard.SetTargetProperty(translateXAnimation, new PropertyPath(TranslateTransform.XProperty));
 
-                // Create a DoubleAnimationUsingPath to move the
-                // rectangle vertically along the path by animating
-                // its TranslateTransform.
-                DoubleAnimationUsingPath translateYAnimation = new DoubleAnimationUsingPath();
-                translateYAnimation.PathGeometry = animationPath;
-                translateYAnimation.Duration = TimeSpan.FromSeconds(2);
+            // same process as above but with y
+            DoubleAnimationUsingPath translateYAnimation = new DoubleAnimationUsingPath();
+            translateYAnimation.PathGeometry = animationPath;
+            translateYAnimation.Duration = TimeSpan.FromSeconds(2);
 
-                // Set the Source property to Y. This makes
-                // the animation generate vertical offset values from
-                // the path information.
-                translateYAnimation.Source = PathAnimationSource.Y;
+            translateYAnimation.Source = PathAnimationSource.Y;
 
-                // Set the animation to target the Y property
-                // of the TranslateTransform named "AnimatedTranslateTransform".
-                Storyboard.SetTargetName(translateYAnimation, "AnimatedTranslateTransform");
-                Storyboard.SetTargetProperty(translateYAnimation, new PropertyPath(TranslateTransform.YProperty));
+            Storyboard.SetTargetName(translateYAnimation, "AnimatedTranslateTransform");
+            Storyboard.SetTargetProperty(translateYAnimation, new PropertyPath(TranslateTransform.YProperty));
 
-                // Create a Storyboard to contain and apply the animations.
-                Storyboard pathAnimationStoryboard = new Storyboard();
-                pathAnimationStoryboard.Children.Add(translateXAnimation);
-                pathAnimationStoryboard.Children.Add(translateYAnimation);
-
-                // Start the storyboard.
-                pathAnimationStoryboard.Completed += PathAnimationStoryboard_Completed;
-                pathAnimationStoryboard.Begin(this);
+            // create and start storyboard
+            Storyboard pathAnimationStoryboard = new Storyboard();
+            pathAnimationStoryboard.Children.Add(translateXAnimation);
+            pathAnimationStoryboard.Children.Add(translateYAnimation);
+            pathAnimationStoryboard.Completed += PathAnimationStoryboard_Completed;
+            pathAnimationStoryboard.Begin(this);
         }
 
         private void PathAnimationStoryboard_Completed(object? sender, EventArgs e)
@@ -261,10 +242,9 @@ namespace Game_Center.Projects.LeapFrogProject
 
         private void StartOver()
         {
-
             //cube 
             FrogCube.RenderTransform = null;
-            PathAnimationStoryboard_Completed(null,new EventArgs());
+            PathAnimationStoryboard_Completed(null, new EventArgs());
             Canvas.SetLeft(FrogCube, 45);
             Canvas.SetTop(FrogCube, 351);
             _endPoint.Y = 0;
@@ -274,10 +254,8 @@ namespace Game_Center.Projects.LeapFrogProject
             Canvas.SetTop(lilyplatform, 396);
 
             //score 
-            Score.Text = $"Score: {score=0}";
-            Tries.Text = $"Tries: {tries=0}";
-
-            
+            Score.Text = $"Score: {score = 0}";
+            Tries.Text = $"Tries: {tries = 0}";
         }
 
         private void ResetGame(object sender, RoutedEventArgs e)
